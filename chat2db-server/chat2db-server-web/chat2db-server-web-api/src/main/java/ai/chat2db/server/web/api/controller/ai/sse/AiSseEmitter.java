333fddd37b60d61045d31fdbe33cec88d334075d
tracenow <liuyuctrlz@gmail.com>
2024-04-20 22:27:52 +0800

    ai sse转文本fix

--- chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/ai/sse/AiSseEmitter.java
+++ chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/ai/sse/AiSseEmitter.java
@@ -1,10 +1,10 @@
 package ai.chat2db.server.web.api.controller.ai.sse;
 
+import com.unfbx.chatgpt.entity.chat.Message;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.lang.reflect.Field;
-import java.util.Objects;
 import java.util.Set;
 
 public class AiSseEmitter extends SseEmitter {
@@ -24,10 +24,10 @@ public class AiSseEmitter extends SseEmitter {
             Set<DataWithMediaType> earlySendAttempts = (Set<DataWithMediaType>) field.get(this);
             StringBuilder sb = new StringBuilder();
             earlySendAttempts.stream()
-                    .filter(earlySendAttempt -> !Objects.equals(earlySendAttempt.getData(), "data:"))
-                    .filter(earlySendAttempt -> !Objects.equals(earlySendAttempt.getData(), "\n\n"))
-                    .forEach(earlySendAttempt -> {
-                        sb.append(earlySendAttempt.getData());
+                    .filter(earlySendAttempt -> earlySendAttempt.getData() instanceof Message)
+                    .map(earlySendAttempt -> (Message) earlySendAttempt.getData())
+                    .forEach(message -> {
+                        sb.append(message.getContent());
                     });
             return sb.toString();
         } catch (NoSuchFieldException e) {
