d7a5b4a6489e02912ff8fab054de6137bcd2cd95
tracenow <liuyuctrlz@gmail.com>
2024-04-29 11:17:35 +0800

    格式化json输入，剔除无效前后缀

--- chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/dashboard/EChartsController.java
+++ chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/dashboard/EChartsController.java
@@ -2,12 +2,15 @@ package ai.chat2db.server.web.api.controller.dashboard;
 
 import ai.chat2db.server.domain.api.model.ImageResult;
 import ai.chat2db.server.domain.api.service.EChartsService;
+import ai.chat2db.server.web.api.controller.dashboard.request.EChartsOptionRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/api/echarts")
 @RestController
 public class EChartsController {
@@ -16,7 +19,10 @@ public class EChartsController {
     private EChartsService eChartsService;
 
     @PostMapping("/generateImage")
-    public ImageResult generateImage(@RequestBody String option) {
-        return eChartsService.generateImage(option);
+    public ImageResult generateImage(@RequestBody EChartsOptionRequest eChartsOptionRequest) {
+        String newOption = eChartsOptionRequest.getOption()
+                .replaceAll("```json", "")
+                .replaceAll("```", "");
+        return eChartsService.generateImage(newOption);
     }
 }
---
+++ chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/dashboard/request/EChartsOptionRequest.java
@@ -0,0 +1,9 @@
+package ai.chat2db.server.web.api.controller.dashboard.request;
+
+import lombok.Data;
+
+@Data
+public class EChartsOptionRequest {
+
+    private String option;
+}
